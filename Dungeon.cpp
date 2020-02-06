79af906f08dfb0800a097f48b7e224abec04c573
DyyLN <dylandhokia@gmail.com>
2020-02-06 15:27:50 +0000

    Basic BSP generation

--- Dungeon.cpp
+++ Dungeon.cpp
@@ -1,26 +1,103 @@
 #include "Dungeon.h"
 
+// Create a dungeon of certain dimensions
 Dungeon::Dungeon(int width, int height){
+    bool allowed = true;
+    if(width < MIN_WIDTH){
+        width = MIN_WIDTH;
+        allowed = false;
+    }
+    if(height < MIN_HEIGHT){
+        height = MIN_HEIGHT;
+        allowed = false;
+    }
+    if(!allowed){
+        printf("Could not create dungeon of required size (Minimum size is %d x %d)", MIN_WIDTH, MIN_HEIGHT);
+    }
+    
     this->width = width;
     this->height = height;
     
-    tiles = new int * [width];
+    tiles = new char * [width];
     for(int i = 0; i < width; i++){
-        tiles[i] = new int [height];
+        tiles[i] = new char [height];
         for(int j = 0; j < height; j++){
             tiles[i][j] = 0;
         }
     }
 }
 
-void Dungeon::generate(){
-    printf("Width : %d, Height : %d\n", width, height);
+char Dungeon::get(int x, int y){
+    if(x < 0 || y < 0 || x >= width || y >= height)return -1;
+    
+    return tiles[x][y];
 }
 
+// Generate the dungeon
+void Dungeon::generate(){
+    printf("Width : %d, Height : %d\n", width, height);
+    
+    // create a border for the dungeon
+    for(int i = 0; i < width; i++){
+        tiles[i][0] = '#';
+        tiles[i][height - 1] = '#';
+    }
+    for(int j = 0; j < height; j++){
+        tiles[0][j] = '#';
+        tiles[width - 1][j] = '#';
+    }
+    
+    std::vector<Room> rooms;
+    
+    // Begin splitting using BSP's
+    Room start = Room(0, 0, width, height);
+    
+    rooms.push_back(start);
+    // Perform splits
+    for(int i = 0; i < 128; i++){
+        int id = rand() % rooms.size();
+        Room r = rooms[id];
+        if(r.w * r.h < 128 || r.w < 12 && r.h < 12)continue;
+        if(i % 2 == 0){
+            //Horizontal split
+            int split_pos = r.w / 4 + rand() % (r.w / 2);
+            r.w -= split_pos;
+            Room r2 = Room(r.x + r.w, r.y, split_pos, r.h);
+            rooms.push_back(r2);
+            rooms[id] = r;
+        }else{
+            // Vertical split
+            int split_pos = r.h / 4 + rand() % (r.h / 2);
+            r.h -= split_pos;
+            Room r2 = Room(r.x, r.y + r.h, r.w, split_pos);
+            rooms.push_back(r2);
+            rooms[id] = r;
+            
+            //r.h /= 2;
+            //Room r2 = Room(r.x, r.y + r.h, r.w, r.h);
+            //rooms.push_back(r2);
+            //rooms[id] = r;
+        }
+        
+    }
+    
+    // Add the rooms to the tilemap
+    for(int i = 0; i < rooms.size(); i++){
+        Room r = rooms[i];
+        printf("Room %d : (%d , %d) %d x %d\n", i, r.x, r.y, r.w, r.h);
+        for(int i = 0; i < r.w; i++){
+            for(int j = 0; j < r.h; j++){
+                if(i == 0 || j == 0)tiles[r.x+i][r.y+j] = '#';
+            }
+        }
+    }
+}
+
+// Print the dungeon to console
 void Dungeon::print_dungeon(){
     for(int j = 0; j < height; j++){
         for(int i = 0; i < width; i++){
-            printf("%d ", tiles[i][j]);
+            printf("%c ", get(i, j));
         }
         printf("\n");
     }
@@ -29,7 +106,7 @@ void Dungeon::print_dungeon(){
 int main(){
     
     printf("Creating dungeon\n");
-    Dungeon dungeon = Dungeon(48, 32);
+    Dungeon dungeon = Dungeon(50, 32);
     
     printf("Generating dungeon\n");
     dungeon.generate();
--- Dungeon.h
+++ Dungeon.h
@@ -1,14 +1,35 @@
 #pragma once
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <vector>
 
 struct Dungeon{
-    int** tiles;
+    static const int MIN_WIDTH = 16, MIN_HEIGHT = 16;
+    
+    char** tiles;
     int width, height;
     
     Dungeon(int width, int height);
     
+    char get(int x, int y);
+    
     void generate();
     
     void print_dungeon();
+};
+
+struct Room{
+    int x;
+    int y;
+    int w;
+    int h;
+    
+    Room(){
+        x = y = w = h = 0;
+    }
+    
+    Room(int x_, int y_, int w_, int h_) : x(x_), y(y_), w(w_), h(h_){
+        
+    }
 };
